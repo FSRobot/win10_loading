2a3130acef4c7033fcda84d1bd1eeca634ff1128
Robot <3237988538@qq.com>
2021-06-07 17:21:09 +0800

    First commit

---
+++ loading.js
@@ -0,0 +1,19 @@
+﻿$(function() {
+        let loading_tag = document.createElement('div');
+        loading_tag.className = "loading hide";
+        let con_tag = document.createElement('i');
+        for (var i = 0; i < 6; i++) {
+            let dot = document.createElement('span');
+            con_tag.append(dot);
+        }
+        loading_tag.append(con_tag);
+        $('body').append(loading_tag);
+    })
+    //加载动画
+const loading = async(falg) => {
+    if (falg) {
+        $('.loading').removeClass('hide');
+    } else {
+        $('.loading').addClass('hide');
+    }
+}
